6fb3f09ad9e3092220d67d03c16a449c9da3ed89
Mou Mazumder <61279910+Mou31@users.noreply.github.com>
2021-11-11 12:12:53 +0530

    Add files via upload

---
+++ 6.js
@@ -0,0 +1,18 @@
+// Given two strings, return true if they are anagrams of one another
+
+function checkStringsAnagram(a, b) {
+    let len1 = a.length;
+    let len2 = b.length;
+    if(len1 !== len2){
+       console.log('Invalid Input');
+       return
+    }
+    let str1 = a.split('').sort().join('');
+    let str2 = b.split('').sort().join('');
+    if(str1 === str2){
+       console.log("True");
+    } else { 
+       console.log("False");
+    }
+ }
+ checkStringsAnagram("indain","ndiani")

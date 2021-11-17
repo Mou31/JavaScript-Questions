a89b3ba630c7716b5c0f19366deacca0289e1ce9
Mou Mazumder <61279910+Mou31@users.noreply.github.com>
2021-11-17 11:38:41 +0530

    Create reduce.js

---
+++ reduce.js
@@ -0,0 +1,6 @@
+// To find count
+
+const countOccurrences = (arr, val) => arr.reduce((a, v) => (v === val ? a + 1 : a), 0);
+console.log(countOccurrences([1, 1, 2, 1, 2, 3], 1));
+console.log(countOccurrences([1, 1, 2, 1, 2, 3], 2));
+console.log(countOccurrences([1, 1, 2, 1, 2, 3], 3));
